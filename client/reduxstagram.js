adcaa3e5df61d04fc4ce4719e9c784a71399abd4
Jonatan Dahl <dahl.jonatan@gmail.com>
2016-08-15 20:23:37 +0200

    your turn!

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// import css yoooo
+import css from './styles/style.styl';
+
+render(<p>hellooo</p>, document.getElementById('root'));
