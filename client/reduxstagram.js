608f1568887d6ff57b53f57a87c9e2e5d9fa5c0d
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-08-15 20:49:20 +0200

    components + started routes

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,8 +1,22 @@
 import React from 'react';
-
 import { render } from 'react-dom';
 
 // import css yoooo
 import css from './styles/style.styl';
 
-render(<p>hellooo</p>, document.getElementById('root'));
+//import components!
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+//import react router dependencies
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+  <Route path="/" component={Main}></Route>
+  </Router>
+)
+
+render(<Main />, document.getElementById('root'));
