c2cb852c640c5c20f42a25631cddfdb40e9790db
Jonatan Dahl <dahl.jonatan@gmail.com>
2016-08-15 21:04:05 +0200

    finished routes and created default store. tag you're it!

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -10,13 +10,15 @@ import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
 //import react router dependencies
-
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 const router = (
   <Router history={browserHistory}>
-  <Route path="/" component={Main}></Route>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
   </Router>
 )
 
-render(<Main />, document.getElementById('root'));
+render(router, document.getElementById('root'));
